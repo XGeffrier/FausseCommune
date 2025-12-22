22d9d84b9c481f407b7630fa7621b2d4916d5a90
XGeffrier <xavier.geffrier@gmail.com>
2025-12-22 21:06:31 +0100

    Catch alternative coordinates ValueError

--- back/game.py
+++ back/game.py
@@ -35,29 +35,35 @@ def get_alternative_coords(coords: tuple[float, float],
                 break
         if ok_alternative_set:
             return alternative_coords
-    raise ValueError("No valid alternative coords found")
+    raise ValueError(
+        f"No valid alternative coords found for coords {coords}, nb_alternatives {nb_alternatives}, and km_min {km_min}.")
 
 
 def play_round(round_ix: int,
                game_seed: str,
-               nb_names: int = 7) -> tuple[
-    tuple[float, float], list[str], list[tuple[float, float]]]:
+               nb_names: int = 7) -> tuple[tuple[float, float], list[str], list[tuple[float, float]]]:
+    for i in range(100):
+        # pick a random model
+        names_by_model_key = StorageClient.get_pre_generated_names_by_model()
+        model_keys = list(names_by_model_key.keys())
+        random.seed(f"{game_seed}_{round_ix}_{i}")
+        model_key = random.choice(model_keys)
 
-    # pick a random model
-    names_by_model_key = StorageClient.get_pre_generated_names_by_model()
-    model_keys = list(names_by_model_key.keys())
-    random.seed(f"{game_seed}_{round_ix}")
-    model_key = random.choice(model_keys)
+        # get names
+        names = sorted(names_by_model_key[model_key]["names"])
+        random.shuffle(names)
+        names = names[:nb_names]
 
-    # get names
-    names = sorted(names_by_model_key[model_key]["names"])
-    random.shuffle(names)
-    names = names[:nb_names]
-
-    # generate alternative coords
-    center_coords = names_by_model_key[model_key]["coords"]
-    nb_alternatives = min(6, round_ix // 3 + 1)
-    km_min = max(75, 500 - round_ix * 25)
-    alternative_coords = get_alternative_coords(center_coords, nb_alternatives, km_min,
-                                                PublicData.get_france_shape(), f"{game_seed}_{round_ix}")
-    return center_coords, names, alternative_coords
+        # generate alternative coords
+        center_coords = names_by_model_key[model_key]["coords"]
+        nb_alternatives = min(6, round_ix // 3 + 1)
+        km_min = max(75, 500 - round_ix * 25)
+        try:
+            alternative_coords = get_alternative_coords(center_coords, nb_alternatives, km_min,
+                                                        PublicData.get_france_shape(), f"{game_seed}_{round_ix}")
+        except ValueError as e:
+            print(e)
+            continue
+        else:
+            return center_coords, names, alternative_coords
+    raise ValueError(f"No valid round found for round_ix {round_ix} and game_seed {game_seed}.")
